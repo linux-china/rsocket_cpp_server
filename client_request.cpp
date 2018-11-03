14c7f86315a5936c7188b613f00f665cab0e8373
linux_china <libing.chen@gmail.com>
2018-11-03 14:28:27 -0700

    rsocket client request/response

---
+++ client_request.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+#include <folly/init/Init.h>
+#include <folly/io/async/ScopedEventBaseThread.h>
+#include <folly/portability/GFlags.h>
+
+#include <rsocket/RSocket.h>
+#include <rsocket/examples/util/ExampleSubscriber.h>
+#include <rsocket/transports/tcp/TcpConnectionFactory.h>
+
+#include <yarpl/Single.h>
+
+using namespace rsocket_example;
+using namespace rsocket;
+using namespace yarpl;
+using namespace yarpl::single;
+
+DEFINE_string(host, "localhost", "host to connect to");
+DEFINE_int32(port, 9898, "host:port to connect to");
+
+int main(int argc, char *argv[]) {
+    FLAGS_logtostderr = true;
+    FLAGS_minloglevel = 0;
+    folly::init(&argc, &argv);
+
+    folly::SocketAddress address;
+    address.setFromHostPort(FLAGS_host, FLAGS_port);
+
+    folly::ScopedEventBaseThread worker;
+    auto client = RSocket::createConnectedClient(
+            std::make_unique<TcpConnectionFactory>(
+                    *worker.getEventBase(), std::move(address)))
+            .get();
+
+    client->getRequester()
+            ->requestResponse(Payload("Jane"))
+            ->subscribe([](Payload p) {
+                std::cout << "Received >> " << p.moveDataToString() << std::endl;
+            });
+
+    // Wait for a newline on the console to terminate the client.
+    std::getchar();
+    return 0;
+}
